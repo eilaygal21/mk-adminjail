b641c2c7afaefaa41ab9a0f4b70916ae9cdf26a8
Entytaiment25 <entytaiment25@outlook.com>
2022-12-24 03:56:11 +0100

    updated to newer Version

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'Eilay#6969'
+description 'Admin Jail Script to jail some Naughty Players'
+
+client_script 'c-main.lua'
+server_script 's-main.lua'
+shared_script 'config.lua'
+
+dependencies {
+    'qb-core'
+}
